5afa7a4b8e93066c580a76145dfef55a4b845b27
Bruno Azisaka Maciel <bruno.azisaka@gmail.com>
2008-10-26 19:43:45 -0200

    Improving the tests

--- test/google_translate_test.rb
+++ test/google_translate_test.rb
@@ -8,6 +8,9 @@ class GoogleTranslateTest < Test::Unit::TestCase
   
   def test_trying_to_translate
     assert_equal("Teste", @gt.translate("Test"))
+  end
+  
+  def test_trying_to_translate_from_portuguese_to_english
     assert_equal("Oi, meu nome é Bruno", @gt.translate("Hi, my name is Bruno"))
   end
 end
--- test/translate_test.rb
+++ test/translate_test.rb
@@ -2,11 +2,16 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../translate'
 
 class GoogleTranslateTest < Test::Unit::TestCase
+  
+  def setup
+    @translate = File.dirname(__FILE__) + '/../translate.rb'
+  end
+  
   def test_trying_to_translate
-    translate = File.dirname(__FILE__) + '/../translate.rb'
-    puts translate
-    assert_equal("Teste", `#{translate} Test`.strip!)
-    
-    assert_equal("Test", `#{translate} Teste pt en`.strip!)
+    assert_equal("Teste", `#{@translate} Test`.strip!)
+  end
+  
+  def test_trying_to_translate_from_portuguese_to_english
+    assert_equal("Test", `#{@translate} Teste pt en`.strip!)
   end
 end
